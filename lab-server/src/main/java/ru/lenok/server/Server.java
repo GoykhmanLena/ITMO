ddbeb5bd7a9752295b5600e4fb38309439c0fc14
GoykhmanLena <lenagojhman@gmail.com>
2025-03-23 12:48:22 +0300

    removed some bad strings

--- lab-server/src/main/java/ru/lenok/server/Server.java
+++ lab-server/src/main/java/ru/lenok/server/Server.java
@@ -2,9 +2,7 @@ package ru.lenok.server;
 
 import ru.lenok.common.Application;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 
 public final class Server {
@@ -14,13 +12,6 @@ public final class Server {
     }
 
     public static void main(String[] args) throws IOException {
-        String lena = "Лена";
-        byte[] bytes = lena.getBytes();
-        byte[] bytes1 = lena.getBytes(StandardCharsets.UTF_8);
-        byte[] bytes2 = lena.getBytes("cp1251");
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
-        int read = inputStream.read();
-
         Application app = new Application(args);
         app.start();
     }
